af3aca3e1d01cbbd9a52bde8c221327e25c856de
Atharva Aiwale <atharva.aiwale28@gmail.com>
2026-06-02 21:40:39 +0530

    Variable assignments and print statements

---
+++ day2_variable.py
@@ -0,0 +1,25 @@
+sentence = "my name is avi "
+print(sentence)
+#output: my name is avi
+
+age = 20
+print(age)
+#output: 20
+
+sarah, bob, mike = 16, 17, 18
+print(sarah)
+print(bob)
+print(mike)
+#output: 16 17 18
+
+sarah = bob = mike = 17
+print(sarah)
+print(bob)
+print(mike)
+#output: 17 17 17
+
+name, age = "avi", 22
+print(name)
+print(age)
+#output: avi 22
+
