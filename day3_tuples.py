be3a331a12f20207a39f979b772b89a21c5a4c5c
Atharva Aiwale <atharva.aiwale28@gmail.com>
2026-06-03 12:44:18 +0530

    Examples of tuple creation and slicing
    Demonstrate tuple usage and operations in Python.

---
+++ day3_tuples.py
@@ -0,0 +1,23 @@
+#Tuples are immutable. They cannot be modified after creation
+
+tuple = ('oranges', 'apples', 'bananas')
+print(tuple)
+
+#------------------------------------------------
+
+tuple = ('oranges', 'apples', 'bananas')
+print(tuple[0])
+
+#------------------------------------------------
+
+tuple = ('oranges', 'apples', 'bananas')
+print(tuple[0:2])
+
+#------------------------------------------------
+
+tuple = ('oranges', 'apples', 'bananas')
+tuple1 = (12,14)
+tuple3 = tuple + tuple1
+print(tuple3)
+
+#-------------------------------------------------
