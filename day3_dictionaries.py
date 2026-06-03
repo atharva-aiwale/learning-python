9adce3ca61be13543756fe5d49dadc8a6798807c
Atharva Aiwale <atharva.aiwale28@gmail.com>
2026-06-03 10:26:06 +0530

    Examples Dictionary operations in Python
    Demonstrate various operations on a dictionary including creation, access, modification, deletion, and length calculation.

---
+++ day3_dictionaries.py
@@ -0,0 +1,31 @@
+students = ['bob', 12, 'rachel', 13, 'emily', 15 ]
+print(students)
+
+#------------------------------------------------------------------------
+
+students = {'bob':12, 'rachel':13, 'emily':15}
+print(students)
+
+#------------------------------------------------------------------------
+
+students = {'bob':12, 'rachel':13, 'emily':15}
+print(students['rachel'])
+
+#------------------------------------------------------------------------
+
+students = {'bob':12, 'rachel':13, 'emily':15}
+students['emily'] = 19  # edit
+print(students)
+
+#-------------------------------------------------------------------------
+
+students = {'bob':12, 'rachel':13, 'emily':15}
+del students['bob']   # delete 
+print(students)
+
+#--------------------------------------------------------------------------
+
+students = {'bob':12, 'rachel':13, 'emily':15}
+print(len(students))  # length or amount or number
+
+#---------------------------------------------------------------------------
